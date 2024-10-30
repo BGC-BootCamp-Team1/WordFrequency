2a3107f6e08f2e59e7b08061909e7d289c19696c
Ting Li <TLi26@slb.com>
2024-10-30 17:09:17 +0800

    refactor: remove long function

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -14,41 +14,52 @@ namespace WordFrequency
             }
             else
             {
-                //split the input string with 1 to n pieces of spaces
-                string[] arr = Regex.Split(inputStr, @"\s+");
-
-                List<Input> inputList = new List<Input>();
-                foreach (var s in arr)
-                {
-                    Input input = new Input(s, 1);
-                    inputList.Add(input);
-                }
-
-                //get the map for the next step of sizing the same word
+                List<Input> inputList = SplitWithSpaces(inputStr);
                 Dictionary<string, List<Input>> map = GetListMap(inputList);
-
-                List<Input> list = new List<Input>();
-                foreach (var entry in map)
-                {
-                    Input input = new Input(entry.Key, entry.Value.Count);
-                    list.Add(input);
-                }
-
-                inputList = list;
-
-                inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
-
-                List<string> strList = new List<string>();
-
-                //stringJoiner joiner = new stringJoiner("\n");
-                foreach (Input w in inputList)
-                {
-                    string s = w.Value + " " + w.WordCount;
-                    strList.Add(s);
-                }
-
-                return string.Join("\n", strList.ToArray());
+                inputList = Resort(map);
+                return GenerateOutputString(inputList);
             }
+        }
+
+        private static List<Input> Resort(Dictionary<string, List<Input>> map)
+        {
+            List<Input> list = new List<Input>();
+            foreach (var entry in map)
+            {
+                Input input = new Input(entry.Key, entry.Value.Count);
+                list.Add(input);
+            }
+
+            list.Sort((w1, w2) => w2.WordCount - w1.WordCount);
+            return list;
+        }
+
+        private static List<Input> SplitWithSpaces(string inputStr)
+        {
+            string[] arr = Regex.Split(inputStr, @"\s+");
+
+            List<Input> inputList = new List<Input>();
+            foreach (var s in arr)
+            {
+                Input input = new Input(s, 1);
+                inputList.Add(input);
+            }
+
+            return inputList;
+        }
+
+        private static string GenerateOutputString(List<Input> inputList)
+        {
+            List<string> strList = new List<string>();
+
+            //stringJoiner joiner = new stringJoiner("\n");
+            foreach (Input w in inputList)
+            {
+                string s = w.Value + " " + w.WordCount;
+                strList.Add(s);
+            }
+
+            return string.Join("\n", strList.ToArray());
         }
 
         private Dictionary<string, List<Input>> GetListMap(List<Input> inputList)
