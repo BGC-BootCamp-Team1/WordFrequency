7e2f999410fc45390a34b02e743f280f46d20dcc
Ting Li <TLi26@slb.com>
2024-10-30 22:45:44 +0800

    factor: optimize method using linq

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -6,31 +6,22 @@ namespace WordFrequency
     {
         public string GetResult(string inputStr)
         {
-            List<WordCountPair> inputList = SplitWithSpaces(inputStr);
-            List<WordCountPair> wordCounts = CountInputWords(inputList);
+            List<WordCountPair> wordCounts = CountInputWords(inputStr);
             return GenerateOutputString(wordCounts);
         }
 
-        private static List<WordCountPair> SplitWithSpaces(string inputStr)
+        private static List<WordCountPair> CountInputWords(string inputStr)
         {
             return Regex.Split(inputStr, @"\s+")
-                .Select( s => new WordCountPair(s, 1))
+                .GroupBy(word => word)
+                .Select(group => new WordCountPair(group.Key, group.Count()))
+                .OrderByDescending(w => w.getCount())
                 .ToList();
         }
 
         private static string GenerateOutputString(List<WordCountPair> outputList)
         {
             return string.Join("\n", outputList.Select(w => w.getWord() + " " + w.getCount()).ToArray());
-        }
-
-        private static List<WordCountPair> CountInputWords(List<WordCountPair> inputList)
-        {
-            List<WordCountPair> inputWordCounts = inputList
-                .GroupBy(input => input.getWord())
-                .Select(group => new WordCountPair(group.Key,group.Count()))
-                .ToList();
-            inputWordCounts.Sort((w1, w2) => w2.getCount() - w1.getCount());
-            return inputWordCounts;
         }
     }
 }
