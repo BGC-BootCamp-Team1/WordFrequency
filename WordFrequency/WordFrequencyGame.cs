63dab98447d5dab06d690c2b90ff5a87dcfa7964
htian15 <htian15@slb.com>
2024-10-30 20:50:45 +0800

    refactor: delete comments

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -6,6 +6,23 @@ namespace WordFrequency;
 
 public class WordFrequencyGame
 {
+    public string GetResult(string inputStr)
+    {
+
+        string[] splitedInput = SplitInputString(inputStr);
+
+        List<WordCount> wordCounts = splitedInput.Select(s => new WordCount(s, 1)).ToList(); ;
+
+
+
+        List<WordCount> resultList = TransformToOutputList(wordCounts);
+
+        List<string> strList = resultList.Select(wc => $"{wc.Word} {wc.Count}").ToList();
+
+        return string.Join("\n", strList);
+
+    }
+
     private static string[] SplitInputString(string inputStr)
     {
         string[] arr = Regex.Split(inputStr, @"\s+");
@@ -15,24 +32,6 @@ public class WordFrequencyGame
         }
 
         return arr;
-
-    }
-    public string GetResult(string inputStr)
-    {
-
-
-
-        string[] splitedInput = SplitInputString(inputStr);
-
-        List<WordCount> wordCounts = splitedInput.Select(s => new WordCount(s, 1)).ToList(); ;
-
-
-        // Get the map for the next step of sizing the same word
-        List<WordCount> resultList = TransformToOutputList(wordCounts);
-
-        List<string> strList = resultList.Select(wc => $"{wc.Word} {wc.Count}").ToList();
-
-        return string.Join("\n", strList);
 
     }
 
@@ -69,74 +68,3 @@ public class WordFrequencyGame
 
 
 
-//namespace WordFrequency
-//{
-//    public class WordFrequencyGame
-//    {
-//        public string GetResult(string inputStr)
-//        {
-//            if (Regex.Split(inputStr, @"\s+").Length == 1)
-//            {
-//                return inputStr + " 1";
-//            }
-//            else
-//            {
-//                //split the input string with 1 to n pieces of spaces
-//                string[] arr = Regex.Split(inputStr, @"\s+");
-
-//                List<Input> inputList = new List<Input>();
-//                foreach (var s in arr)
-//                {
-//                    Input input = new Input(s, 1);
-//                    inputList.Add(input);
-//                }
-
-//                //get the map for the next step of sizing the same word
-//                Dictionary<string, List<Input>> map = GetListMap(inputList);
-
-//                List<Input> list = new List<Input>();
-//                foreach (var entry in map)
-//                {
-//                    Input input = new Input(entry.Key, entry.Value);
-//                    list.Add(input);
-//                }
-
-//                inputList = list;
-
-//                inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
-
-//                List<string> strList = new List<string>();
-
-//                //stringJoiner joiner = new stringJoiner("\n");
-//                foreach (Input w in inputList)
-//                {
-//                    string s = w.Value + " " + w.WordCount;
-//                    strList.Add(s);
-//                }
-
-//                return string.Join("\n", strList.ToArray());
-//            }
-//        }
-
-//        private Dictionary<string, List<Input>> GetListMap(List<Input> inputList)
-//        {
-//            Dictionary<string, List<Input>> map = new Dictionary<string, List<Input>>();
-//            foreach (var input in inputList)
-//            {
-//                //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-//                if (!map.ContainsKey(input.Value))
-//                {
-//                    List<Input> arr = new List<Input>();
-//                    arr.Add(input);
-//                    map.Add(input.Value, arr);
-//                }
-//                else
-//                {
-//                    map[input.Value].Add(input);
-//                }
-//            }
-
-//            return map;
-//        }
-//    }
-//}
